6f19dd638efad7f2fa85998a5c9834dbd543a4a7
GouravGarg84 <gouravgarg08042002@gmail.com>
2022-05-26 12:22:02 +0530

    My First Commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mysql = require('mysql');
+
+// Create connection
+const db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '123456'
+});
+
+// Connect
+db.connect((err) => {
+    if(err){
+        console.log('err');
+    }
+    console.log('MySql Connected...');
+});
+
+const app = express();
+
+// Create DB
+app.get('/createdb', (req, res) => {
+    let sql = 'CREATE DATABASE nodemysql';
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Database created...');
+    });
+});
+
+
+
+app.listen('3000', () => {
+    console.log('Server started on port 3000');
+});
